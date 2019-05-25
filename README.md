461c7e7cdc5a8670a49adaf0a0b25d00ce1133ef
rosowskimik <m.rosowski1@wp.pl>
2019-05-25 18:27:51 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Profile Scroller using iterator
---
+++ app.js
@@ -0,0 +1,27 @@
+//Placeholer data
+const data = [
+	{
+		name: 'John Doe',
+		age: 32,
+		gender: 'male',
+		lookingfor: 'female',
+		location: 'Boston, MA',
+		image: 'https://randomuser.me/api/portraits/men/69.jpg'
+	},
+	{
+		name: 'Jane Smith',
+		age: 26,
+		gender: 'female',
+		lookingfor: 'male',
+		location: 'Miami, FL',
+		image: 'https://randomuser.me/api/portraits/women/34.jpg'
+	},
+	{
+		name: 'William Johnson',
+		age: 28,
+		gender: 'male',
+		lookingfor: 'female',
+		location: 'Lynn, MA',
+		image: 'https://randomuser.me/api/portraits/men/17.jpg'
+	}
+];
