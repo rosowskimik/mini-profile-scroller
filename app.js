996f3b173c060299ed8b9ee54448ac3697949645
rosowskimik <m.rosowski1@wp.pl>
2019-05-25 18:56:07 +0200

    Display profiles

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const data = [
 	{
 		name: 'John Doe',
 		age: 32,
-		gender: 'male',
+		gender: 'Male',
 		lookingfor: 'female',
 		location: 'Boston, MA',
 		image: 'https://randomuser.me/api/portraits/men/69.jpg'
@@ -11,7 +11,7 @@ const data = [
 	{
 		name: 'Jane Smith',
 		age: 26,
-		gender: 'female',
+		gender: 'Female',
 		lookingfor: 'male',
 		location: 'Miami, FL',
 		image: 'https://randomuser.me/api/portraits/women/34.jpg'
@@ -19,9 +19,52 @@ const data = [
 	{
 		name: 'William Johnson',
 		age: 28,
-		gender: 'male',
+		gender: 'Male',
 		lookingfor: 'female',
 		location: 'Lynn, MA',
 		image: 'https://randomuser.me/api/portraits/men/17.jpg'
 	}
 ];
+
+const profile = profileIterator(data);
+
+//Display first profile
+nextProfile();
+
+//Next button event
+document.querySelector('#next').addEventListener('click', nextProfile);
+
+//Next Profile
+function nextProfile() {
+	const currentProfile = profile.next().value;
+
+	if(currentProfile !== undefined) {
+		document.querySelector('#profile-image').innerHTML = `
+			<img src="${currentProfile.image}">
+		`;
+		document.querySelector('#profile-desc').innerHTML = `
+			<ul class="list-group">
+				<li class="list-group-item">Name: ${currentProfile.name}</li>
+				<li class="list-group-item">Age: ${currentProfile.age}</li>
+				<li class="list-group-item">Location: ${currentProfile.location}</li>
+				<li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+			</ul>
+		`;
+	} else {
+		//Reload after finished
+		window.location.reload();
+	}
+}
+
+//Profile Iterator
+function profileIterator(profiles){
+	let nextIndex = 0;
+
+	return {
+		next: function() {
+			return nextIndex < profiles.length ?
+			{ value: profiles[nextIndex++], done: false } :
+			{ done: true }
+		}
+	}
+}
